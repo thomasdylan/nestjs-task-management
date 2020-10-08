64e329f00ac7e737b1ef697db54e3ee73cbbc6b1
Dylan Thomas <thomasdylan76@gmail.com>
2020-10-07 20:25:31 -0600

    Added validation pipe for filtering and search

--- src/tasks/pipes/task-status-validation.pipe.ts
+++ src/tasks/pipes/task-status-validation.pipe.ts
@@ -1,12 +1,28 @@
-import { ArgumentMetadata, PipeTransform } from '@nestjs/common';
-
-import { PipeTransform } from '@nestjs/common';
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  PipeTransform,
+} from '@nestjs/common';
+import { TaskStatus } from '../task.model';
 
 export class TaskStatusValidationPipe implements PipeTransform {
+  readonly allowedStatuses = [
+    TaskStatus.OPEN,
+    TaskStatus.IN_PROGRESS,
+    TaskStatus.DONE,
+  ];
   transform(value: any, metadata: ArgumentMetadata) {
-    console.log('value', value);
-    console.log('metadata', metadata);
+    value = value.toUpperCase();
+
+    if (!this.isStatusValid(value)) {
+      throw new BadRequestException('"${value}" is an invalid status');
+    }
 
     return value;
+  }
+
+  private isStatusValid(status: any) {
+    const idx = this.allowedStatuses.indexOf(status);
+    return idx !== -1;
   }
 }
