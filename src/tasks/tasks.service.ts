df45a0391c93e6b4d9fa90526091059d45aab17a
Dylan Thomas <thomasdylan76@gmail.com>
2020-10-10 20:18:21 -0600

    Finished refactoring for task repository

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -44,17 +44,7 @@ export class TasksService {
     return this.TaskRepository.createTask(CreateTaskDto);
   }
 
-  // createTask(createTaskDto: CreateTaskDto): Task {
-  //   const { title, description } = createTaskDto;
-  //   const task: Task = {
-  //     id: uuid.v1(),
-  //     title,
-  //     description,
-  //     status: TaskStatus.OPEN,
-  //   };
-  //   this.tasks.push(task);
-  //   return task;
-  // }
+
   // deleteTask(id: string): void {
   //   const found = this.getTaskById(id);
   //   this.tasks = this.tasks.filter(task => task.id !== found.id);
