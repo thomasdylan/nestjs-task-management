b0efd2983a95f77f3a3ee6975fd5d88760a1b8ca
Dylan Thomas <thomasdylan76@gmail.com>
2020-10-11 18:49:56 -0600

    jwt strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserRepository } from './user.reposity';
+import { JwtStrategy } from './jwt.strategy';
 
 
 @Module({
@@ -19,6 +20,13 @@ import { UserRepository } from './user.reposity';
     TypeOrmModule.forFeature([UserRepository]),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    JwtStrategy,
+  ],
+  exports: [
+    JwtStrategy,
+    PassportModule,
+  ],
 })
 export class AuthModule {}
